1271d24bf8cff0a9725bafbd3b7ee0ceb08e86da
T1 <>
2018-03-07 21:12:29 +0800

    Initial commit

---
+++ app/src/main/java/com/wangqian/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.wangqian.fragmentbestpractice;
+
+/**
+ * Created by t1 on 2018/3/7.
+ */
+
+public class News {
+    private String tittle;
+    private String content;
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
